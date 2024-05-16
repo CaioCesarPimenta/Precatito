1de8e7038f733a012d531b5a018b4578d794c571
CaioCesarPimenta <107492015+CaioCesarPimenta@users.noreply.github.com>
2024-05-16 20:40:36 -0300

    Update Precatito_modelagem.py

--- Precatito_modelagem.py
+++ Precatito_modelagem.py
@@ -4,19 +4,11 @@ import os
 import streamlit as st
 import pandas as pd
 
-haders = {
-    "autorisation": st.secrets["OPENAI_API_KEY"],
-    "autorisation": st.secrets["csv_file_path"],
-    "content-type": "application/json",
-    "content-type": "application/json"
-}
-
 def CSV():
     csv_file_path = st.secrets["csv_file_path"]
     OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
     
     if os.path.exists(csv_file_path):
-
         cola = ['Periodos.Periodo', 'Reserva de produtos', 'PROC', 'CLASSIFICAÇÃO DO PERÍODO', 'CLASSIFICAÇÃO', 'AUTORA', 'DESMEMBRAMENTO', 'Nº DO DESMEMBRAMENTO', 'CLASSIFICAÇÃO DO ATIVO', 'HC', 'HS', 'EXPEDIÇÃO OU ESTIMATIVA DE EXPEDIÇÃO', 'INÍCIO', 'FIM', 'NÚMERO DO PRECATÓRIO', 'TIPO DE CESSÃO', 'CESSÃO AUTORA', 'CESSÃO AUTORA2', 'AUTORA CEDIDO PARA', 'CESSÃO HC', 'HC CEDIDO PARA', 'CESSÃO HS', 'HS CEDIDO PARA', 'STATUS DO ATIVO', 'ESTIMATIVA DE DATA DE PAGAMENTO', 'Valor atualizado Bruto 1', 'Desconto Previdenciário 1', 'HC + imposto 1', 'IR - RRA 1', 'Pgto Parcial e Prioridade 1', 'Recolhimento Legal 1', 'Valor atualizado Liquido 1', 'CJP Total', 'Outros Total', 'BOFA', 'Autoras Total', 'GAE Total', 'Atlas 7 Total', 'Droom Total', 'DISP Autoras', 'DISP HC', 'DISP HS', 'DISP HC e HS', 'DISP TOTAL']
         df = pd.read_csv(csv_file_path, names=cola, sep=';')
         df['CESSÃO HC'] = df['CESSÃO HC'].astype(float)
@@ -24,18 +16,26 @@ def CSV():
         
         user_question = st.text_input("Pergunte ao Precatito sobre a modelagem: ")
 
-        if user_question is not None and user_question != "":
+        if user_question:
             with st.spinner(text="In progress..."):
                 # Temporarily save the DataFrame as a CSV file
                 temp_csv_file = "temp.csv"
                 df.to_csv(temp_csv_file, index=False)
                 
+                # Create the OpenAI LLM instance
+                llm = OpenAI(api_key=OPENAI_API_KEY, temperature=0, model="gpt-3.5-turbo")
+                
                 # Pass the CSV file path to create_csv_agent
-                agent = create_csv_agent(OpenAI(api_key=OPENAI_API_KEY,temperature=0,model="gpt-3.5-turbo-0125"), temp_csv_file, names=cola, sep=';')
+                agent = create_csv_agent(llm, temp_csv_file, column_names=cola, sep=';')
                 
                 # Delete the temporary CSV file after use
                 os.remove(temp_csv_file)
                 
-                st.write(agent.run(user_question))
+                # Run the agent with the user question
+                response = agent.run(user_question)
+                st.write(response)
     else:
         st.error("CSV file not found.")
+
+if __name__ == "__main__":
+    CSV()
