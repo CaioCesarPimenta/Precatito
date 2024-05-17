915c931ae81a95c54d2a42fc23eb5926b616306b
CaioCesarPimenta <107492015+CaioCesarPimenta@users.noreply.github.com>
2024-05-16 21:05:36 -0300

    Update Precatito_modelagem.py

--- Precatito_modelagem.py
+++ Precatito_modelagem.py
@@ -14,34 +14,26 @@ def CSV():
     OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
     
     if os.path.exists(csv_file_path):
-        column_names = ['Periodos.Periodo', 'Reserva de produtos', 'PROC', 'CLASSIFICAÇÃO DO PERÍODO', 'CLASSIFICAÇÃO', 'AUTORA', 'DESMEMBRAMENTO', 'Nº DO DESMEMBRAMENTO', 'CLASSIFICAÇÃO DO ATIVO', 'HC', 'HS', 'EXPEDIÇÃO OU ESTIMATIVA DE EXPEDIÇÃO', 'INÍCIO', 'FIM', 'NÚMERO DO PRECATÓRIO', 'TIPO DE CESSÃO', 'CESSÃO AUTORA', 'CESSÃO AUTORA2', 'AUTORA CEDIDO PARA', 'CESSÃO HC', 'HC CEDIDO PARA', 'CESSÃO HS', 'HS CEDIDO PARA', 'STATUS DO ATIVO', 'ESTIMATIVA DE DATA DE PAGAMENTO', 'Valor atualizado Bruto 1', 'Desconto Previdenciário 1', 'HC + imposto 1', 'IR - RRA 1', 'Pgto Parcial e Prioridade 1', 'Recolhimento Legal 1', 'Valor atualizado Liquido 1', 'CJP Total', 'Outros Total', 'BOFA', 'Autoras Total', 'GAE Total', 'Atlas 7 Total', 'Droom Total', 'DISP Autoras', 'DISP HC', 'DISP HS', 'DISP HC e HS', 'DISP TOTAL']
-        df = pd.read_csv(csv_file_path, names=column_names, sep=';')
+
+        cola = ['Periodos.Periodo', 'Reserva de produtos', 'PROC', 'CLASSIFICAÇÃO DO PERÍODO', 'CLASSIFICAÇÃO', 'AUTORA', 'DESMEMBRAMENTO', 'Nº DO DESMEMBRAMENTO', 'CLASSIFICAÇÃO DO ATIVO', 'HC', 'HS', 'EXPEDIÇÃO OU ESTIMATIVA DE EXPEDIÇÃO', 'INÍCIO', 'FIM', 'NÚMERO DO PRECATÓRIO', 'TIPO DE CESSÃO', 'CESSÃO AUTORA', 'CESSÃO AUTORA2', 'AUTORA CEDIDO PARA', 'CESSÃO HC', 'HC CEDIDO PARA', 'CESSÃO HS', 'HS CEDIDO PARA', 'STATUS DO ATIVO', 'ESTIMATIVA DE DATA DE PAGAMENTO', 'Valor atualizado Bruto 1', 'Desconto Previdenciário 1', 'HC + imposto 1', 'IR - RRA 1', 'Pgto Parcial e Prioridade 1', 'Recolhimento Legal 1', 'Valor atualizado Liquido 1', 'CJP Total', 'Outros Total', 'BOFA', 'Autoras Total', 'GAE Total', 'Atlas 7 Total', 'Droom Total', 'DISP Autoras', 'DISP HC', 'DISP HS', 'DISP HC e HS', 'DISP TOTAL']
+        df = pd.read_csv(csv_file_path, names=cola, sep=';')
         df['CESSÃO HC'] = df['CESSÃO HC'].astype(float)
         df['Valor atualizado Liquido 1'] = df['Valor atualizado Liquido 1'].astype(float)
         
         user_question = st.text_input("Pergunte ao Precatito sobre a modelagem: ")
 
         if user_question is not None and user_question != "":
-            with st.spinner(text="Em progresso..."):
+            with st.spinner(text="In progress..."):
                 # Temporarily save the DataFrame as a CSV file
                 temp_csv_file = "temp.csv"
                 df.to_csv(temp_csv_file, index=False)
                 
                 # Pass the CSV file path to create_csv_agent
-                agent = create_csv_agent(
-                    OpenAI(api_key=OPENAI_API_KEY, temperature=0, model="gpt-3.5-turbo-0125"),
-                    temp_csv_file,
-                    names=column_names,
-                    sep=';',
-                    options={'endpoint': 'v1/chat/completions'}  # Alterando o endpoint para v1/chat/completions
-                )
+                agent = create_csv_agent(OpenAI(temperature=0), temp_csv_file, names=cola, sep=';')
                 
                 # Delete the temporary CSV file after use
                 os.remove(temp_csv_file)
                 
-                response = agent.run(user_question)
-                st.write(response)
+                st.write(agent.run(user_question))
     else:
-        st.error("Arquivo CSV não encontrado.")
-
-CSV()
+        st.error("CSV file not found.")
