ed401bec596852906d9058743aa73e6c2190b329
CaioCesarPimenta <107492015+CaioCesarPimenta@users.noreply.github.com>
2024-05-19 19:23:46 -0300

    Update Tela_incio.py

--- Tela_incio.py
+++ Tela_incio.py
@@ -1,14 +1,14 @@
 import streamlit as st
-import Precatito_processos as processo
-import Precatito_modelagem as modelagem
+import skills.Precatito_processos as processo
+import skills.Precatito_modelagem as modelagem
+import skills.modelagem_consulta as consulta
 import openai
-
 
 def main():
     st.header("Precatito Goul🦉")
    
     st.sidebar.title("HABILIDADES")
-    skills = st.sidebar.selectbox("modelos",["Precatito Processos","Precatito modelagem"])
+    skills = st.sidebar.selectbox("modelos",["Precatito Processos","Precatito modelagem","Consulta Modelagem"])
 
     if skills == 'Precatito Processos':
         st.header("📄")
@@ -17,6 +17,10 @@ def main():
     elif skills == 'Precatito modelagem':
         st.header("📈")
         modelagem.CSV()
+    
+    elif skills == "Consulta Modelagem":
+        st.header("📈")
+        consulta.consulta_modelagem()
         
 if __name__ == "__main__":
     main()
