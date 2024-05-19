23f03f7e51f312749b5b6dd6c33b0c8c86335c58
CaioCesarPimenta <107492015+CaioCesarPimenta@users.noreply.github.com>
2024-05-19 19:30:11 -0300

    Update modelagem_consulta.py

--- modelagem_consulta.py
+++ modelagem_consulta.py
@@ -9,8 +9,14 @@ import streamlit as st
 
 def consulta_modelagem():
 
+    haders = {
+    "autorisation": st.secrets["csv_file_path"],
+    "content-type": "application/json"
+}
+
+
     cola = ["Periodos.Periodo",	"Reserva de produtos",	"PROC",	"Bloco",	"TRANCHE",	"CLASSIFICAÇÃO DO PERÍODO",	"CLASSIFICAÇÃO",	"NÚMERO DO PROCESSO",	"AUTORA",	"DESMEMBRAMENTO",	"Nº DO DESMEMBRAMENTO",	"CPF",	"CLASSIFICAÇÃO DO ATIVO",	"STATUS AUTORA",	"DATA FALECIMENTO",	"HABILITAÇÃO",	"DATA DA HABILITAÇÃO",	"NOME DO HABILITADO",	"Tese",	"HC",	"HS",	"EXPEDIÇÃO OU ESTIMATIVA DE EXPEDIÇÃO",	"INÍCIO",	"FIM",	"NÚMERO DO PRECATÓRIO",	"DATA DO CÁLCULO",	"VALOR TOTAL",	"PRINCIPAL",	"JUROS",	"DESCONTO PREVIDENCIÁRIO",	"CUSTAS",	"TAXA JUDICIÁRIA",	"DATA DO PAGAMENTO DE PRIORIDADE",	"VALOR DO PAGAMENTO DE PRIORIDADE",	"TIPO DE CESSÃO",	"CESSÃO AUTORA",	"CESSÃO AUTORA2",	"AUTORA CEDIDO PARA",	"CESSÃO HC",	"HC CEDIDO PARA",	"CESSÃO HS",	"HS CEDIDO PARA",	"STATUS DO ATIVO",	"OBS",	"PROBALIDADE DO ÊXITO DA EXECUÇÃO",	"OPORTUNIDADE",	"ESTIMATIVA DE DATA DE PAGAMENTO",	"DATA DO MANDADO DE PAGAMENTO",	"VALOR DO MANDADO DE PAGAMENTO",	"DATA DO RECIBO DE LEVANTAMENTO",	"VALOR LEVANTADO",	"IMPOSTO RETIDO",	"Atualização Jurídica",	"Droom_Digital",	"Adv",	"Ver",	"Razões",	"Posição na Fila",	"Situação na Fila",	"Valor atualizado Bruto 1",	"Desconto Previdenciário 1",	"HC + imposto 1",	"IR - RRA 1",	"Pgto Parcial e Prioridade 1",	"Recolhimento Legal 1",	"Valor atualizado Liquido 1",	"CJP Total",	"Outros Total",	"BOFA",	"Autoras Total",	"GAE Total",	"Atlas 7 Total",	"Droom Total",	"DISP Autoras",	"DISP HC",	"DISP HS",	"DISP HC e HS",	"DISP TOTAL"]
-    csv_file_path=r"C:\Users\ntbgae056\Desktop\modelagem maio.csv"
+    csv_file_path=st.secrets["csv_file_path"]
 
     df = pd.read_csv(csv_file_path, names=cola, sep=';',decimal=".")
     df = df.drop(index=0)
